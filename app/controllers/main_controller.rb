2740721d36b8b983cc3c31bd5217bf6e3ae98be5
Ethan Jones <joneetha@oregonstate.edu>
2019-12-04 14:18:05 -0800

    do a bunch of stuff

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,9 @@
 class MainController < ApplicationController
   def index
   end
+
+  def charge
+    tok = params['stripeToken']
+    Stripe.api_key = 'vagd-flyy-ygob-virr-uwlv'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'main/index'
+  post '/main', to: 'main#charge'
   root 'main#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
